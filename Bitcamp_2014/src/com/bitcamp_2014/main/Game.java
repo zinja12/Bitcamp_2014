7f09f8fdd11fa6d620e2632dd4875368d76c82f8
zinja12 <x3zinja12@gmail.com>
2014-04-04 13:28:09 -0400

    +Added main method

---
+++ Bitcamp_2014/src/com/bitcamp_2014/main/Game.java
@@ -0,0 +1,14 @@
+package com.bitcamp_2014.main;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Game {
+	public static void main(String args[]){
+		System.out.println("Testing...");
+	}
+}
