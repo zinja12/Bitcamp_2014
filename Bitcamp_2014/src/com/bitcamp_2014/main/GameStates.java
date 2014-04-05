bafbca8642eb7e26e38d9af1bb98475157f3fe29
zinja12 <x3zinja12@gmail.com>
2014-04-05 08:43:36 -0400

    +Got sound to work in a continuous loop //Everything works! //Fuck, I'm tired...

--- Bitcamp_2014/src/com/bitcamp_2014/main/GameStates.java
+++ Bitcamp_2014/src/com/bitcamp_2014/main/GameStates.java
@@ -1,10 +1,15 @@
 package com.bitcamp_2014.main;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.Graphics2D;
+import java.io.File;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 
 import com.bitcamp_2014.level.Level;
 import com.bitcamp_2014.menus.EndMenu;
@@ -23,14 +28,31 @@ public class GameStates {
 	StartMenu startMenu;
 	EndMenu endMenu;
 	
-	public Clip sound = loadClip("res/gameAudio_01.wav");
-	
 	public GameStates(){
 		current_state = GameState.TITLE;
 		
 		level = new Level();
 		startMenu = new StartMenu();
 		endMenu = new EndMenu();
+		
+		try {
+		    File yourFile = new File("res/gameAudio_01.wav");
+		    AudioInputStream stream;
+		    AudioFormat format;
+		    DataLine.Info info;
+		    Clip clip;
+
+		    stream = AudioSystem.getAudioInputStream(yourFile);
+		    format = stream.getFormat();
+		    info = new DataLine.Info(Clip.class, format);
+		    clip = (Clip) AudioSystem.getLine(info);
+		    clip.open(stream);
+		    clip.loop(Clip.LOOP_CONTINUOUSLY);
+		}
+		catch (Exception e) {
+		    //Do not do anything
+			e.printStackTrace();
+		}
 	}
 	
 	public void update(){
@@ -60,20 +82,4 @@ public class GameStates {
 			endMenu.render(g);
 		}
 	}
-	
-	public Clip loadClip(String filename){
-		Clip clip = null;
-		
-		try{
-			AudioInputStream audioIn = AudioSystem.getAudioInputStream(getClass().getResource(filename));
-			clip = AudioSystem.getClip();
-			clip.open(audioIn);
-		} catch(Exception e){
-			e.printStackTrace();
-		}
-		
-		return clip;
-	}
-	
-	
 }
