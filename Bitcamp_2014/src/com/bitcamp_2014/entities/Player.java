1a8e477fad669dcbb2f9d62573a1de1105768567
jaygrl216 <jwashin7@terpmail.umd.edu>
2014-04-05 00:22:12 -0500

    Added Player class

--- Bitcamp_2014/src/com/bitcamp_2014/entities/Player.java
+++ Bitcamp_2014/src/com/bitcamp_2014/entities/Player.java
@@ -1,5 +1,35 @@
 package com.bitcamp_2014.entities;
 
+import java.awt.Graphics2D;
+import java.awt.Point;
+
+import com.bitcamp_2014.input.Input;
+
+
 public class Player {
-	
+	Point position;
+	int angle = 180;
+
+	public Player(){
+		position = new Point(0,0);
+
+	}
+
+	public void update(){
+		if(Input.left){
+			angle--;
+			
+		} else if (Input.right){
+			angle++;
+
+		}
+
+	}
+
+	public void render(Graphics2D g){
+		g.drawRect(position.x, position.y, 50, 50);
+		g.rotate(angle);
+		
+
+	}
 }
--- Bitcamp_2014/src/com/bitcamp_2014/entities/PuzzlePiece.java
+++ Bitcamp_2014/src/com/bitcamp_2014/entities/PuzzlePiece.java
@@ -1,5 +1,18 @@
 package com.bitcamp_2014.entities;
 
+import java.awt.Graphics;
+
 public class PuzzlePiece {
 
+	public PuzzlePiece(){
+		
+	}
+	
+	public void update(){
+		
+	}
+	
+	public void render(Graphics g){
+		
+	}
 }
