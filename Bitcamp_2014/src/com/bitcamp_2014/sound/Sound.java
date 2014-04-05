261be693ae18794c2bfe8fd59abd3ba06e7147fe
zinja12 <x3zinja12@gmail.com>
2014-04-05 07:34:38 -0400

    +Adding sound things

--- Bitcamp_2014/src/com/bitcamp_2014/sound/Sound.java
+++ Bitcamp_2014/src/com/bitcamp_2014/sound/Sound.java
@@ -1,5 +1,32 @@
 package com.bitcamp_2014.sound;
 
-public class Sound {
+import java.applet.Applet;
+import java.applet.AudioClip;
+import java.net.URL;
 
+public class Sound {
+	private AudioClip sound;
+	private URL songPath;
+	
+	public Sound(String fileName){
+		try{
+			songPath = new URL(fileName);
+			sound = Applet.newAudioClip(songPath);
+		} catch(Exception e){
+			//Do nothing
+			e.printStackTrace();
+		}
+	}
+	
+	public void playSound(){
+		sound.loop();
+	}
+	
+	public void stopSound(){
+		sound.stop();
+	}
+	
+	public void playSoundOnce(){
+		sound.play();
+	}
 }
